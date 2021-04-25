279f4e30bfb926d996ca3cd664459e729789c0f6
Milton <orellanamilton@hotmail.com>
2021-04-24 21:51:36 -0300

    Update... + React router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import * as React from "react";
-import "@fontsource/roboto"
+import "@fontsource/roboto";
+import "@fontsource/lato";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 /* COMPONENTS */
 import Home from "./pages/Home";
+import About from "./pages/AboutMe";
+import Experience from "./pages/Experience"
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Box  minHeight='100vh' bg="warm.100">
+      <Router>
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/About">
+            <About />
+          </Route>
+          <Route exact path="/Experience">
+            <Experience/>
+          </Route>
+        </Switch>
+      </Router>
+    </Box>
   );
 }
 
