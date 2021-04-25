d2124266ef3c5632da72145a92c121ea3348d8fb
Milton <orellanamilton@hotmail.com>
2021-04-24 22:00:09 -0300

    Delete "Text"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading, Flex, Text} from "@chakra-ui/react";
+import { Heading, Flex} from "@chakra-ui/react";
 
 export default function Header() {
   return (
