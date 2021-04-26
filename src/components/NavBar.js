afcbcbb1e85046a5a67a16fe09a938c8d8cabe0a
Milton <orellanamilton@hotmail.com>
2021-04-26 16:05:07 -0300

    Logo and favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,24 +9,59 @@ import {
   Flex,
   Box,
   Image,
-  Heading,
+  Heading
 } from "@chakra-ui/react";
-import { Link as RouteLink} from "react-router-dom";
+import { motion, useViewportScroll } from "framer-motion";
+
+import { Link as RouteLink } from "react-router-dom";
 
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import logo from "../images/Logo.svg";
+import logo from "../images/Logo.png";
 
 export default function NavBar() {
+  const MotionBox = motion(Box);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const { scrollYProgress } = useViewportScroll()
+
   return (
-    <Flex as="nav" pos={"sticky"} w="100%" justify="space-between" zIndex="1" px="4" py="2" alignItems="center" bg={["warm.100", "warm.100", "transparent"]} top="0">
-      <RouteLink as="logo" exact to="/">
+    <Flex
+      as="nav"
+      pos={"sticky"}
+      w="100%"
+      justify="space-between"
+      zIndex="1"
+      px="4"
+      py="2"
+      alignItems="center"
+      bg={["warm.100", "warm.100", "transparent"]}
+      top="0"
+    >
+      {/*       <RouteLink as="logo" exact to="/">
         <Image boxSize="50px" objectFit="fit" src={logo} alt="logo"  />
+      </RouteLink> */}
+
+      <RouteLink as="logo" exact to="/">
+        <MotionBox
+          animate={{
+            scale: [1, 1, 1, 1, 1],
+            rotate: [0, 0, -10, 10, 0],
+          }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <Image boxSize="60px" objectFit="fit" src={logo} alt="logo" />
+        </MotionBox>
       </RouteLink>
 
       <Box>
-        <AiOutlineMenu size="30" onClick={onOpen} cursor="pointer" aria-label="Menu button"/>
+        <AiOutlineMenu
+          size="30"
+          onClick={onOpen}
+          cursor="pointer"
+          aria-label="Menu button"
+        />
         <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
           <DrawerOverlay>
             <DrawerContent bg="warm.100">
